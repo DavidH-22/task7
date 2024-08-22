8c8a89cd793b76841d9a2be11d9c5d37cb1ac961
David <davidhartono231002@gmail.comgit config --global user.email davidhartono231002@gmail.com>
2024-08-22 17:56:52 +0700

    menambah form tamu dan tombol back di jam serta melakukan gitignore

---
+++ formulir.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+    <?php 
+        $namaD = $_GET["namadepan"];
+        $namaB = $_GET["namabelakang"];
+
+        echo "nama depan" . $namaD;
+        echo "<br/>"
+        echo "nama belakang". $namaB;
+        echo "<br/>"
+    
+
+        ?>
+
+
+</body>
+</html>
